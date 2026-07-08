d17d40ff25236b4f225b706f282cd3cd4cd4bec4
julia <juliaarlovskaya@mail.ru>
2026-07-08 07:45:12 +0300

    картинки соответствуют id курса

--- src/constants/courseImages.ts
+++ src/constants/courseImages.ts
@@ -1,9 +1,9 @@
 export const courseImages: Record<string, string> = {
-  ypox9r: "/image 5.jpg", 
-  "6i67sm": "/image 6.jpg", 
-  ab1c3f: "/image 7.jpg", 
-  kfpq8e: "/image 8.jpg", 
-  q02a6i: "/image 9.jpg", 
+  ypox9r: "/image 6.jpg",
+  "6i67sm": "/image 7.jpg",
+  ab1c3f: "/image 9.jpg",
+  kfpq8e: "/image 5.jpg",
+  q02a6i: "/image 8.jpg",
 };
 
 export const defaultCourseImage = "/image 5.jpg";
